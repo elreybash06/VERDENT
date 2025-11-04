be110551c78b5b5e76671c91042aa3409278251a
Mohammed Bashiru Bonsu-Bandoh <bashirubonsubandoh@gmail.com>
2025-11-04 21:21:09 +0000

    new changes 2.0

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import express from 'express';
+import path from "path";
+import { fileURLToPath} from "url";
+
 const app = express();
 const port = 3000;
 
-app.use(express.static("Public"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "Public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "Views"));
+
 app.get("/",(req,res) => {
     res.render("index.ejs")
 })
-
-app.get("/contact", (req, res) => {
-    res.send("0544025323")
-});
-
-app.get("/about", (req, res) => {
-    res.send("my name is mohammed bashiru bonsu-bandoh")
-});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
